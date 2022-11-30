642dee59f3de4655ed099d1b84ac4078d06ca50d
FrumpleQuest <tomas.barros@sansano.usm.cl>
2022-11-30 20:39:48 -0300

    Movimientos encontrados

--- funciones.h
+++ funciones.h
@@ -10,6 +10,19 @@ struct Nodo{
     Nodo* der;
 };
 
+struct Move{
+    int start_position;
+    int end_position;
+    int kill;
+};
+
+struct Movimientos{
+    Move* listaMovimientos;
+    int length; 
+};
+
+//Necesitaremos una función que transforme coordenadas de start_position/end_position en algo como A8
+
 void printBoard(int* board, int N){
     for (int j = 0; j < N; j++){
         for(int i = 0; i < N; i++){
@@ -23,5 +36,82 @@ void printBoard(int* board, int N){
 float eval(int* board, int N);
 
 //Función que retorna una lista con movimientos
+Movimientos* generarMovimientos(int* board, int N, int n_fichas){ //n_fichas (de 1 jugador) nos servirá para dejar de buscar cuando hayamos procesado todas las fichas
+    int colum;
+    int fila;
+
+    int aux_numero_fichas = n_fichas;
+
+    Movimientos* movimientos = new Movimientos; //Acotamos la cantidad de movimientos (cantidad de fichas * 2)
+    movimientos -> listaMovimientos = new Move[n_fichas * 2];
+    movimientos -> length = 0;
+    for(int i = 0; i < N*N; i++){
+        colum = i % N;
+        fila = i / N;
+
+        //Encontramos una ficha aliada
+        if (board[colum + N*fila] == 2){
+            //Si es que estamos en el borde izquierdo
+            if (colum == 0){
+                //Si derecha arriba esta libre, agregamos el movimiento
+                if (board[colum + N*fila + 1 - N] == 0){
+                    (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila + 1 - N,-1};
+                    movimientos -> length++;
+                }
+                //Si derecha arriba ocupada pero ficha y puede comersela, es un movimiento
+                else if ((board[colum + N*fila + 1 - N] == 1) && (colum + N*fila + 2 - 2*N > 0)){
+                    if (board[colum + N*fila + 2 - 2*N] == 0){
+                        (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila + 2 - 2*N,colum + N*fila + 1 - N};
+                        movimientos -> length++;
+                    }
+                }
+            }
+            //Si es que estamos en el borde derecho
+            else if(colum == N-1){
+                //Si izquierda arriba esta libre, agregamos el movimiento
+                if (board[colum + N*fila - 1 - N] == 0){
+                    (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila - 1 - N,-1};
+                    movimientos -> length++;
+                }
+                //Si izquierda arriba ocupada pero ficha y puede comersela, es un movimiento
+                else if ((board[colum + N*fila - 1 - N] == 1) && (colum + N*fila - 2 - 2*N > 0)){
+                    if (board[colum + N*fila - 2 - 2*N] == 0){
+                        (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila - 2 - 2*N,colum + N*fila - 1 - N};
+                        movimientos -> length++;
+                    }
+                }
+            }
+            else{
+                //Si izquierda arriba esta libre, agregamos el movimiento
+                if (board[colum + N*fila - 1 - N] == 0){
+                    (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila - 1 - N,-1};
+                    movimientos -> length++;
+                }
+                //Si izquierda arriba ocupada pero ficha y puede comersela, es un movimiento
+                else if ((board[colum + N*fila - 1 - N] == 1) && (colum + N*fila - 2 - 2*N > 0)){
+                    if (board[colum + N*fila - 2 - 2*N] == 0){
+                        (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila - 2 - 2*N,colum + N*fila - 1 - N};
+                        movimientos -> length++;
+                    }
+                }
+                //Si derecha arriba esta libre, agregamos el movimiento
+                if (board[colum + N*fila + 1 - N] == 0){
+                    (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila + 1 - N,-1};
+                    movimientos -> length++;
+                }
+                //Si derecha arriba ocupada pero ficha y puede comersela, es un movimiento
+                else if ((board[colum + N*fila + 1 - N] == 1) && (colum + N*fila + 2 - 2*N > 0)){
+                    if (board[colum + N*fila + 2 - 2*N] == 0){
+                        (movimientos -> listaMovimientos)[movimientos -> length] = {colum + N*fila,colum + N*fila + 2 - 2*N,colum + N*fila + 1 - N};
+                        movimientos -> length++;
+                    }
+                }
+            }
+        aux_numero_fichas--;
+        if (aux_numero_fichas == 0) break;
+        }
+    }
+    return movimientos;
+}
 
 //Funcion que escoge un movimiento al azar
---
+++ proyecto_cuda.cu
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <cuda_runtime.h>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+using namespace std;
+
+__global__ void kernel(float* Ain, float* Aout, int M, int N, float deltax){
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (tid < M*N){
+        int tid_left = (!(tid % N)) ? (tid + N - 1) : (tid - 1);
+        int tid_right = (!(tid % (N-1))) ? (tid - N + 1) : (tid + 1);
+        Aout[tid] = Ain[tid] + (Ain[tid_right] - Ain[tid_left])/(2*deltax);
+    }
+}
+
+/* ----   Codigo Principal ---- */
+ 
+
+int main(int argc, char** argv) {
+
+    // Variables que trabajaremos 
+    int N = 8;
+    int* board = new int[N*N];
+    int n_fichas = 0;
+    
+    //Construccion de tablero inicial
+    int filas_con_fichas = (N-2)/2;
+    for (int i = 0; i < N*N; i++){
+        board[i] = 0;
+        if (i/N < filas_con_fichas){
+            if ((i/N)%2){
+                if (!(i%2)) board[i] = 1;
+            }
+            else{
+                if (i%2) board[i] = 1;
+            } 
+        }
+        else if (i/N > filas_con_fichas + 1){
+            if (!(i/N%2)){
+                if (i%2){
+                    board[i] = 2;
+                    n_fichas++;
+                } 
+            }
+            else{
+                if (!(i%2)){
+                    n_fichas++;
+                    board[i] = 2;
+                }
+            } 
+        }
+    }
+    board[33] = 1;
+    //Printeamos tablero para revisar si todo esta bien
+    printBoard(board, N);
+    Movimientos* movimientos = generarMovimientos(board, N, n_fichas);
+    for (int i = 0; i < movimientos -> length; i++){
+        printf("%d-%d\n",(movimientos -> listaMovimientos)[i].start_position,(movimientos -> listaMovimientos)[i].end_position);
+    }
+    
+    //Juego versión CPU
+
+
+
+
+
+
+/*
+    bool flag_finalizado = false;
+    while(!flag_finalizado){
+        printBoard(board, N);
+        //Generar movimientos de jugador
+        //Permitir a jugador escoger movimientos
+
+        //Generar movimientos contrincante 
+        //Escoger movimientos contrincante (IA)
+        //if (JuegoFinalizado()) flag_finalizado = true;
+    }
+
+
+
+
+
+
+
+    
+
+
+
+    
+    
+	float dt;
+	int gs, bs = 256;
+	gs = (int)ceil((float) N*N/bs);
+
+    
+    clock_t start = clock();
+    //JogodeDamas(Ain, Aout, M, N, deltax);
+    clock_t end = clock();
+    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("Tiempo CPU: %f [ms]\n",seconds*1000);
+   
+    
+
+
+    float* AinGPU;
+    float* AoutGPU;
+
+    cudaMalloc((void**)&AinGPU, N * M * sizeof(float));
+    cudaMalloc((void**)&AoutGPU, N * M * sizeof(float));
+
+    cudaEvent_t e1, e2;
+    cudaEventCreate(&e1);
+    cudaEventCreate(&e2);
+    cudaEventRecord(e1);
+    
+
+    cudaMemcpy(AinGPU, Ain, N * M * sizeof(float), cudaMemcpyHostToDevice);
+    kernel << <gs, bs >> > (AinGPU, AoutGPU, M, N, deltax);
+    cudaMemcpy(Aout, AoutGPU, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+    Ain = Aout;
+    
+
+    cudaEventRecord(e2);
+    cudaEventSynchronize(e2);
+    cudaEventElapsedTime(&dt, e1, e2);
+    std::cout << "Tiempo GPU Sin Streams: " << dt << " [ms]" << std::endl;
+
+	cudaFree(AinGPU); cudaFree(AoutGPU);
+	delete[] Ain;
+    
+    */
+}
